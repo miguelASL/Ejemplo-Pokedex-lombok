f023b8d1183cfa4b9507fdcbb71ca4332a46814f
Miguel Ángel Sarmiento Levy <112961729+miguelASL@users.noreply.github.com>
2023-09-15 15:54:40 +0200

    Delete repository directory

--- repository/PokeRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.pokedex.repository;
-
-import com.example.pokedex.entities.PokedexEntities;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PokeRepositorio extends CrudRepository<PokedexEntities, Long> {
-}
